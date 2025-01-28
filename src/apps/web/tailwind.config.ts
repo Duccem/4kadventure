dd42e0b0563a3160095c9d02996f272b7777488e
Jose Veliz <ducen29@gmail.com>
2025-01-27 21:11:42 -0400

    Init project

---
+++ src/apps/web/tailwind.config.ts
@@ -0,0 +1,2 @@
+export { config as default } from '@4k/tailwind-config/config';
+
---
+++ src/packages/ui/eslint.config.mjs
@@ -0,0 +1,4 @@
+import { config } from "@4k/eslint-config/react-internal";
+
+/** @type {import("eslint").Linter.Config} */
+export default config;
